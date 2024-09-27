75cea439e5a6a9655f5a99a7b12c15f94d4f6c00
gavkhar16 <khabibullaevagavkhar@gmail.com>
2024-09-27 16:56:34 +0500

    two

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Header } from "../components/Header";
-import { AppLabl } from "../components/AppLable";
 import { AppButton } from "../components/AppButton";
 import { AppContainer } from "../components/AppContainer";
 import { AppPassword } from "../components/AppPassword";
