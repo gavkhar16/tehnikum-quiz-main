07256118fb32fdc0e548905d29a634a0990638f4
gavkhar16 <khabibullaevagavkhar@gmail.com>
2024-09-30 15:12:20 +0500

    ex

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import "./styles/main.css";
 const App = () => {   
   return (     
     <div className="App">      
-       <Welcome /> 
+       {/* <Welcome />  */}
 
-      {/* <Registration/> */}
+       <Registration/> */
     </div>   
   ); 
 };  
